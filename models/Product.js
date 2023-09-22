d32d1d4ea3afa4406851b5863cc15bd3b875d2aa
aedelfrid <almck51@gmail.com>
2023-09-22 14:18:16 -0300

    added category and product routes

--- models/Product.js
+++ models/Product.js
@@ -22,13 +22,17 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
-      validate: DECIMAL
+      validate: {
+        isDecimal:true
+      }
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
-      validate:NUMERIC
+      validate: {
+        isNumeric: true
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,51 +4,62 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const id = req.params.id
+    const productData = await Product.findByPk(id, {
+      include: [{ model: Category }, { model: Tag }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
+router.post('/', async (req, res) => {
+  try {
+    const { product_name, price, stock, category_id, tag_ids } = req.body;
+    const product = await Product.create({
+      product_name: product_name,
+      price: price,
+      stock: stock,
+      category_id: category_id
     });
+
+    if (tag_ids.length) {
+      const productTagIdArr = tag_ids.map((tag_ids) => {
+        return {
+          product_id: product.id,
+          tag_id: tag_ids,
+        };
+      });
+
+      const productTagIds = await ProductTag.bulkCreate(productTagIdArr);
+      return res.status(200).json({ product, productTagIds });
+    } else {
+      res.status(200).json(product);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -92,8 +103,25 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const productID = req.params.id;
+    const deletedProduct = await Product.destroy({
+      where: {
+        id: productID
+      }
+
+    })
+    const deletedProdTags = await ProductTag.destroy({
+      where: {
+        product_id: productID
+      }
+    })
+    res.status(200).json(`Requested Productand Product Tags have been deleted.`)
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
 });
 
 module.exports = router;
